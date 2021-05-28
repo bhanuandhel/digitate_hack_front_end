ce7ce3385c37fcd0ba2b9867e79b98c6b34a9e6f
bhanu123456 <bhanupratap636@gmail.com>
2021-05-28 18:23:44 +0530

    footer  setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavbarContainer from './components/Layout/Navbar/NavbarContainer';
 import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/Layout/Footer';
 
 const App = () => {
     return (
         <BrowserRouter>
             <NavbarContainer />
+            <Footer />
         </BrowserRouter>
     );
 };
--- src/components/Layout/Navbar/NavbarView.js
+++ src/components/Layout/Navbar/NavbarView.js
@@ -4,7 +4,9 @@ const NavbarView = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/"><img src="assets/img/slack_logo.png" className="rounded-circle" width="40" height="40" /></Link>
+                <Link className="navbar-brand" to="/">
+                    <img src="assets/img/slack_logo.png" className="rounded-circle" width="40" height="40" />
+                    </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
