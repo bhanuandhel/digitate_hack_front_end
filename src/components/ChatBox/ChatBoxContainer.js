af5bcf3caa1d33cadad69772ec28e501899744a2
bhanu123456 <bhanupratap636@gmail.com>
2021-06-19 16:08:31 +0530

    message breakup setup

--- src/components/ChatBox/ChatBoxContainer.js
+++ src/components/ChatBox/ChatBoxContainer.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ChatBoxView from './ChatBoxView'
 const ChatBoxContainer = () => {
+    const [message, setMessage] = useState('');
+
+    const handleChange = (e) =>{
+        setMessage(e.target.value)
+        }
+
+    const handleSubmit = (e) =>{
+            e.preventDefault()
+            console.log(message)
+        }
     return (
-        <ChatBoxView />
+        <ChatBoxView
+        {...message} 
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        />
     )
 }
 
